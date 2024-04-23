7577fffde7482014f962ce3ebd087473c24e838f
staniszewskia <staniszewski01adam@gmail.com>
2024-04-23 11:36:22 +0200

    Backend skeleton done

--- CorFlow-Backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ CorFlow-Backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51eb4db3a3ccebbff7705a055ec184a1bb080d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a0183c796e7bcd972a5ee022ffd8604a88ad737")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
