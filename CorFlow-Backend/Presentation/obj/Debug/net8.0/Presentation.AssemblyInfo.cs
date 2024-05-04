4bff7684ee1e1129a3c590eb7b3259b7d1bb944a
AStaniszewski <staniszewski01adam@gmail.com>
2024-05-04 10:26:08 +0200

    Added gitignore

--- CorFlow-Backend/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ CorFlow-Backend/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7577fffde7482014f962ce3ebd087473c24e838f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f70936a6d0651b791cf8971432aa5c4829efd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
