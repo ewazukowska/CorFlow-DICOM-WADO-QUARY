8689f62b254a6e6c1475a72f084e8a1cd9283196
staniszewskia <staniszewski01adam@gmail.com>
2024-05-14 09:26:53 +0200

    Hierarchy change

--- CorFlow-Backend/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ CorFlow-Backend/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bff7684ee1e1129a3c590eb7b3259b7d1bb944a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49414e27c32d5cebe33de07edb36c7d2db532247")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
